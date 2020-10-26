3691ae545045c704beb5f6cc122d0db5198f88e1
Kartagis <tolga@ozses.net>
2020-10-26 13:58:14 +0300

    Created Index and edited App.js to reflect this

---
+++ src/screens/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Index</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default Index
